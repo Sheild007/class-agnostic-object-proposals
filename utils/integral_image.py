7c31366fcb5bff7bcbbaad98fac54fe8d12355ea
Usman <usman.muneer720@gmail.com>
2025-12-24 10:48:05 +0500

    feat:compute_integral_image

---
+++ utils/integral_image.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class IntegralImage:
+    def __init__(self, image: np.ndarray):
+        self.original_shape = image.shape
+        self.ii = self.compute_integral_image(image)
+
+    def compute_integral_image(self, img: np.ndarray) -> np.ndarray:
+      
+        # II(x,y) = sum of all pixels above and to the left of (x,y)
+       
+        # V(x,y) = I(x,y) + V(x-1,y)
+        row_integral = np.cumsum(img.astype(np.float64), axis=0)
+
+        # II(x,y) = I(x,y) + II(x-1,y) + II(x,y-1) - II(x-1,y-1)
+        ii = np.cumsum(row_integral, axis=1)
+        
+        # Pading with zero top row and left column to simplify(avoid checks) sum calculations
+        pad_width = ((1, 0), (1, 0)) 
+        ii_padded = np.pad(ii, pad_width, mode='constant', constant_values=0)
+        
+        return ii_padded
+
+    
